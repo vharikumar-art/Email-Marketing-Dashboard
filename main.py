737dd4096e0c30e7f969e746cbab0c37109f36e5
hari kumar <harikumar809816@gmail.com>
2026-04-10 16:04:42 +0530

    Implement strict CORS with slash handling

--- main.py
+++ main.py
@@ -124,6 +124,7 @@ app.add_middleware(
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origins_regex=r"https://.*\.vercel\.app",
 )
 # app.add_middleware(
 #     CORSMiddleware,
